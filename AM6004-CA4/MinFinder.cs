e2e4aef82589c0a0b55f66cc26a23c5ccb1167ad
Neeraj <joseph.neeraj@gmail.com>
2019-04-19 21:18:19 +0100

    Backup

---
+++ AM6004-CA4/MinFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM6004_CA4
+{
+    class MinFinder
+    {
+        static public double[] GradDescent(Func<double[], double> g, Func<double[], double[]> gp,
+            double[] x, double alpha, int dim, double Tol, int N)
+        {
+
+        }
+    }
+}
--- AM6004-CA4/Program.cs
+++ AM6004-CA4/Program.cs
@@ -10,6 +10,24 @@ namespace AM6004_CA4
     {
         static void Main(string[] args)
         {
+            double alpha = 0.01;
+            double Tol = 0.001;
+            double N = 1000;
+            double[] x0 = {-1, 2}; // Sure ??
+
+            MinFinder.GradDescent(BealesFunction, BealesPartialDerivative, x0, alpha, dim, Tol, N);
+        }
+
+        private static double BealesFunction(double[] x)
+        {
+            return Math.Pow((1.5 - x[0] + x[0] * x[1]), 2) 
+                + Math.Pow((2.25 - x[0] + x[0] * x[1] * x[1]), 2)
+                + Math.Pow((2.625 - x[0] + x[0] * x[1] * x[1] * x[1]), 2);
+        }
+
+        private static double[] BealesPartialDerivative(double[] x)
+        {
+            return new double[] { };
         }
     }
 }
