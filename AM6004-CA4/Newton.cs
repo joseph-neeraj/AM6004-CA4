0f095b9f2919985c801dc1e2ae057c245845a18c
Neeraj <joseph.neeraj@gmail.com>
2019-04-23 17:53:59 +0100

    Backup

---
+++ AM6004-CA4/Newton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM6004_CA4
+{
+    class Newton
+    {
+        static public double[] NonLinearShooting(Func<double, double, double, double> f,
+Func<double, double, double, double> fy, Func<double, double, double, double> fyp,
+double a, double b, double alpha, double beta, int N, int MaxIt, double Tol)
+        {
+            return new double[] { };
+        }
+    }
+}
